24422a6199a2e91acff055f3eb9c3ac5e2c07714
Berend Iwema <berend.nl@gmail.com>
2019-05-09 15:28:07 +0200

    Missed the controller

---
+++ app/Http/Controllers/AvailabilityController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use App\Consumer\Weddingplanner\LocationConsumer;
+use App\Consumer\Weddingplanner\BabsConsumer;
+
+class AvailabilityController extends Controller {
+
+
+    public function availability() {
+        return view(
+            ' beschikbaarheid',
+            [
+                'locations' => (new LocationConsumer())->getDetailedLocationList()
+                , 'babs' =>  (new BabsConsumer())->getDetailedBabsList()
+                , 'message' => ''
+            ]
+        );
+    }
+
+    public function testLocations() {
+        $oConsumer = new LocationConsumer();
+        var_dump($oConsumer->getDetailedLocationList());
+    }
+
+    public function testBabs() {
+        $oConsumer = new BabsConsumer();
+        var_dump($oConsumer->getDetailedBabsList());
+    }
+}
